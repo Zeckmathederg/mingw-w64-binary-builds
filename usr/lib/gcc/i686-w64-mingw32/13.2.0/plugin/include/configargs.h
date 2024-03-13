9776c78875e90b1550fbd6e726fe1a7393de5f4c
Zeckmathederg <zeckmaisreal@gmail.com>
2024-03-13 01:27:32 -0600

    added all built software

---
+++ usr/lib/gcc/i686-w64-mingw32/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/usr --libexecdir=/usr/lib --target=i686-w64-mingw32 --enable-languages=ada,c,c++ --enable-shared --enable-static --enable-threads=posix --enable-fully-dynamic-string --enable-libstdcxx-time=yes --enable-libstdcxx-filesystem-ts=yes --with-system-zlib --enable-cloog-backend=isl --enable-lto --enable-libgomp --disable-multilib --enable-checking=release --disable-sjlj-exceptions --with-dwarf2";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" } };
